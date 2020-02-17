867f585e37fa5d43751c8db3eec134861a49fa0c
yuboon <yuboon@126.com>
2020-02-17 08:58:50 +0800

    删除无用代码

--- 2020/突破CRUD_万能树构造工具类封装/source/springboot-tree/src/main/java/com/yuboon/springboot/tree/controller/TreeController.java
+++ 2020/突破CRUD_万能树构造工具类封装/source/springboot-tree/src/main/java/com/yuboon/springboot/tree/controller/TreeController.java
@@ -1,4 +1,4 @@
-package com.yuboon.springboot.tree;
+package com.yuboon.springboot.tree.controller;
 
 import cn.hutool.core.collection.CollectionUtil;
 import com.yuboon.springboot.tree.entity.MenuEntity;
@@ -27,15 +27,7 @@ public class TreeController {
     public List<TreeNode> getTreeNodes(){
         //List<MenuEntity> menuData = jdbcTemplate.query("select * from t_menu", new BeanPropertyRowMapper<>(MenuEntity.class));
 
-
-        List<MenuEntity> menuEntityList = CollectionUtil.newArrayList();
-        menuEntityList.add(new MenuEntity("1","0","系统管理","sys","/sys"));
-        menuEntityList.add(new MenuEntity("11","1","用户管理","user","/sys/user"));
-        menuEntityList.add(new MenuEntity("111","11","用户添加","userAdd","/sys/user/add"));
-        menuEntityList.add(new MenuEntity("2","0","店铺管理","store","/store"));
-        menuEntityList.add(new MenuEntity("21","2","商品管理","shop","/shop"));
-
-
+        List<MenuEntity> menuEntityList = createMenuEntities();
         List<TreeNode> treeNodes = TreeUtil_2_0.build(menuEntityList, "0", new TreeUtil_2_0.Convert<MenuEntity, TreeNode>() {
             @Override
             public void convert(MenuEntity object, TreeNode treeNode) {
@@ -51,24 +43,7 @@ public class TreeController {
 
     @RequestMapping("/getTreeNodes2")
     public List<TreeNodeMap> getTreeNodes2(){
-        List<MenuEntity> menuEntityList = CollectionUtil.newArrayList();
-        menuEntityList.add(new MenuEntity("1","0","系统管理","sys","/sys"));
-        menuEntityList.add(new MenuEntity("11","1","用户管理","user","/sys/user"));
-        menuEntityList.add(new MenuEntity("111","11","用户添加","userAdd","/sys/user/add"));
-        menuEntityList.add(new MenuEntity("2","0","店铺管理","store","/store"));
-        menuEntityList.add(new MenuEntity("21","2","商品管理","shop","/shop"));
-
-        /*List<TreeNodeMap> treeNodes = TreeUtil_4_0.build(menuEntityList, "0", new TreeUtil_4_0.Convert<MenuEntity, TreeNodeMap>() {
-            @Override
-            public void convert(MenuEntity object, TreeNodeMap treeNode) {
-                treeNode.setId(object.getId());
-                treeNode.setParentId(object.getPid());
-                treeNode.setCode(object.getCode());
-                treeNode.setName(object.getName());
-                treeNode.extra("extra1","123");
-            }
-        });*/
-
+        List<MenuEntity> menuEntityList = createMenuEntities();
         TreeNodeConfig treeNodeConfig = new TreeNodeConfig();
         treeNodeConfig.setParentIdKey("pid");
         List<TreeNodeMap> treeNodes = TreeUtil_4_0.build(menuEntityList, "0",treeNodeConfig,new TreeUtil_4_0.Convert<MenuEntity, TreeNodeMap>() {
@@ -83,6 +58,16 @@ public class TreeController {
         });
 
         return treeNodes;
+    }
+
+    private List<MenuEntity> createMenuEntities() {
+        List<MenuEntity> menuEntityList = CollectionUtil.newArrayList();
+        menuEntityList.add(new MenuEntity("1", "0", "系统管理", "sys", "/sys"));
+        menuEntityList.add(new MenuEntity("11", "1", "用户管理", "user", "/sys/user"));
+        menuEntityList.add(new MenuEntity("111", "11", "用户添加", "userAdd", "/sys/user/add"));
+        menuEntityList.add(new MenuEntity("2", "0", "店铺管理", "store", "/store"));
+        menuEntityList.add(new MenuEntity("21", "2", "商品管理", "shop", "/shop"));
+        return menuEntityList;
     }
 
 }
